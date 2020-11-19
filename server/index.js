9602a3e904a3ac3e7b37901c3588526da562c321
Orr Goren <orrgorenn@gmail.com>
2020-11-19 11:39:46 +0200

    Client update

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postsRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use('/posts', postsRoutes);
+
+const CONNECTION_URL = "mongodb+srv://orrgorenn:Base64encoding!@chatapp.4qkeh.mongodb.net/database?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 2058;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
